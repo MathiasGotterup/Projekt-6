f26bc59179d8e998b8c6eda8bb181b4e6018715d
lejlacola <95622931+lejlacola@users.noreply.github.com>
2022-05-17 18:22:21 +0200

    Update lejla.js

--- javascript/lejla.js
+++ javascript/lejla.js
@@ -6,24 +6,24 @@ openPopupButtons.forEach(button => {
     button.addEventListener('click', () => {
         const popup = document.querySelector(button.dataset.popupTarget)
         openPopup(popup)
-    })
-})
+    });
+});
 
 closePopupButtons.forEach(button => {
     button.addEventListener('click', () => {
         const popup = button.closest('.popup')
         closePopup(popup)
-    })
-})
+    });
+});
 
 function openPopup(popup) {
     if (popup == null) return
     popup.classList.add('active')
     overlay.classList.add('active')
-}
+};
 
 function closePopup(popup) {
     if (popup == null) return
     popup.classList.remove('active')
     overlay.classList.remove('active')
-}
+};
