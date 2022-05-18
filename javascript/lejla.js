cececb9915d6108e1593d29d2399eb70fed381e9
lejlacola <95622931+lejlacola@users.noreply.github.com>
2022-05-18 12:41:04 +0200

    forskellige resultater..

--- javascript/lejla.js
+++ javascript/lejla.js
@@ -28,3 +28,21 @@ function closePopup(popup) {
     overlay.classList.remove('active')
 };
 
+var valueList = document.getElementById('valueList');
+var text = '<span> Du kan spare </span>';
+var listArray = [];
+
+var checkboxes = document.querySelectorAll('.checkbox');
+
+for(var checkbox of checkboxes) {
+    checkbox.addEventListener('click', function(){
+        if(this.checked == true) {
+            listArray.push(this.value);
+            valueList.innerHTML = text + listArray.join('/');
+        }
+        else {
+            listArray = listArray.filter(e => e !== this.value);
+            valueList.innerHTML = text + listArray.join('/');
+        }
+    })
+}
