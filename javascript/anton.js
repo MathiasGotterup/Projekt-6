2f85f7902d755bfb8d99cdea2ac4de083148efa4
AntonWarming <95622901+AntonWarming@users.noreply.github.com>
2022-05-19 16:16:23 +0200

    færdig burger menu :D

--- javascript/anton.js
+++ javascript/anton.js
@@ -1,27 +1,50 @@
 window.onload = function() {
   if (window.matchMedia("(min-width: 1000px)").matches) {
-    document.getElementById('mobil').style.display = "none";
-    document.getElementById('header').style.display = "block";
+    document.getElementById('toggle').style.display = "none";
+    document.getElementById('menuheader').style.display = "block";
   }
   else {
-    document.getElementById('mobil').style.display = "block";
-    document.getElementById('header').style.display = "none";
+    document.getElementById('toggle').style.display = "flex";
+    document.getElementById('menuheader').style.display = "none";
   }}
 
 window.addEventListener("resize", function() {
   if (window.matchMedia("(min-width: 1000px)").matches) {
-    document.getElementById('mobil').style.display = "none";
-    document.getElementById('header').style.display = "block";
+    document.getElementById('toggle').style.display = "none";
+    document.getElementById('menuheader').style.display = "block";
   }
   else {
-    document.getElementById('mobil').style.display = "block";
-    document.getElementById('header').style.display = "none";
+    document.getElementById('toggle').style.display = "flex";
+    document.getElementById('menuheader').style.display = "none";
   }})
 
-function clickhamburger() {
-  if (document.getElementById('mobilmenu').style.display = "none") {
-    document.getElementById('mobilmenu').style.display = "block";
-  }
-  else {
-    document.getElementById('mobilmenu').style.display = "none";
-  }}
+
+let menuTog = document.querySelector('.menu-toggle');
+let nav = document.querySelector('.nav');
+let navVenstre = document.querySelector('.nav-venstre');
+let navRight = document.querySelector('.nav-right');
+let navLinks = Array.from(document.querySelectorAll('.nav-link'));
+
+menuTog.addEventListener('click', () => {
+  menuTog.classList.toggle('active');
+  navRight.classList.toggle('active');
+  navVenstre.classList.toggle('active');
+  nav.classList.toggle('active');
+
+
+    navRight.classList.remove('active');
+
+    if(menuTog.classList.contains('active')){
+      setTimeout(()=>{
+        navRight.classList.add('active');
+      },100)
+
+      for(let i = 0; i < navLinks.length; i++){
+        navLinks[i].classList.remove('active');
+        setTimeout(()=>{
+          navLinks[i].classList.add('active')
+        }, i * 100)
+      }
+    }
+
+})
