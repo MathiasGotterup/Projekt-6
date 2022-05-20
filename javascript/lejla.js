b5aa23fc279ab13249fc09b2b36aabad7c877e68
lejlacola <95622931+lejlacola@users.noreply.github.com>
2022-05-20 10:01:47 +0200

    forskellig resultat svar

--- javascript/lejla.js
+++ javascript/lejla.js
@@ -1,9 +1,24 @@
-const openPopupButtons = document.querySelectorAll('[data-popup-target]');
-const closePopupButtons = document.querySelectorAll('[data-luk-button]');
-const overlay = document.getElementById('overlay');
+let listArray = [];
+let openPopupButtons = document.querySelectorAll('[data-popup-target]');
+let closePopupButtons = document.querySelectorAll('[data-luk-button]');
+let overlay = document.getElementById('overlay');
 
 openPopupButtons.forEach(button => {
     button.addEventListener('click', () => {
+        if (listArray.length == 0) {
+            popuptext = "20-25%";
+        } else if (listArray.length == 1) {
+            popuptext = "10-20%";
+        } else if (listArray.length == 2) {
+            popuptext = "10-20%";
+        } else if (listArray.length == 3) {
+            popuptext = "10-20%";
+        } else if (listArray.length == 4) {
+            popuptext = "10-20%";
+        } else if (listArray.length == 5) {
+            popuptext = "5-10%";
+        }
+        document.getElementById('valueList').innerHTML = popuptext;
         const popup = document.querySelector(button.dataset.popupTarget)
         openPopup(popup)
     });
@@ -28,21 +43,15 @@ function closePopup(popup) {
     overlay.classList.remove('active')
 };
 
-var valueList = document.getElementById('valueList');
-var text = '<span> Du kan spare </span>';
-var listArray = [];
+let checkboxes = document.querySelectorAll('.checkbox');
 
-var checkboxes = document.querySelectorAll('.checkbox');
-
-for(var checkbox of checkboxes) {
+for(let checkbox of checkboxes) {
     checkbox.addEventListener('click', function(){
         if(this.checked == true) {
             listArray.push(this.value);
-            valueList.innerHTML = text + listArray.shift() + listArray.join('');
         }
         else {
             listArray = listArray.filter(e => e !== this.value);
-            valueList.innerHTML = text + '20-25%';
         }
     })
 };
