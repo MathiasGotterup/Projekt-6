fe9a1576e74edadb63a2a4073eaae0195f41f102
MathiasGotterup <mat-g@live.dk>
2022-05-23 09:20:20 +0200

    validator

--- javascript/trine.js
+++ javascript/trine.js
@@ -9,11 +9,11 @@ function plusSlides(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("slideshow");
-  if (n > slides.length) {slideIndex = 1}
-  else if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {slideIndex = 1;}
+  else if (n < 1) {slideIndex = slides.length;}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  
+
   slides[slideIndex-1].style.display = "block";
 }
