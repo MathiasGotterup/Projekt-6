1e9502fc56ab44276008f3a947f04affc3650569
MathiasGotterup <mat-g@live.dk>
2022-05-29 21:14:49 +0200

    Done

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
 function GetSelected() {
-    //Create an Array.
+
     var selected = new Array();
 
-    //Reference the Table.
-    var tblFruits = document.getElementById("knapperalle");
+    var checkListe = document.getElementById("checkListe");
 
-    //Reference all the CheckBoxes in Table.
-    var chks = tblFruits.getElementsByTagName("INPUT");
+    var chks = checkListe.getElementsByTagName("INPUT");
 
-    // Loop and push the checked CheckBox value in Array.
     for (var i = 0; i < chks.length; i++) {
         if (chks[i].checked) {
             selected.push(chks[i].value);
         }
     }
-    //Display the selected CheckBox values.
     if (selected.length > 0) {
         alert("Du forstod: " + selected.join(","));
     }
